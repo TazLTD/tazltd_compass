63d9789dbd62bf0b78b7c62d28d7775ae3cce16d
Taz <zqanakiev@gmail.com>
2023-05-26 21:42:31 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+fx_version "bodacious"
+games {"gta5"}
+lua54 'yes'
+author "LTD"
+version "2.0"
+
+client_script 'client.lua'
+files {
+    "html/index.html",
+    "html/css/*.css",
+    "html/img/*.png",
+    "html/js/*.js",
+ }
+
+ui_page 'html/index.html'
